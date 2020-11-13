ae9c771d8b10c618883c34b22b28fdbbe4ab0d2d
GormNobodoran <gremiodemke@yahoo.es>
2020-11-13 21:54:04 +0100

    01-Changed magic numbers with constants

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,38 +1,47 @@
 # -*- coding: utf-8 -*-
 
+
 class GildedRose(object):
+    __HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros"
+    __BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+    __AGED_BRIE = "Aged Brie"
+    __MIN_QUALITY = 0
+    __MAX_QUALITY = 50
+    __SELL_IN_LONG = 11
+    __SELL_IN_MID = 6
+    __SELL_TODAY = 0
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != self.__AGED_BRIE and item.name != self.__BACKSTAGE_PASSES:
+                if item.quality > self.__MIN_QUALITY:
+                    if item.name != self.__HAND_OF_RAGNAROS:
                         item.quality = item.quality - 1
             else:
-                if item.quality < 50:
+                if item.quality < self.__MAX_QUALITY:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
+                    if item.name == self.__BACKSTAGE_PASSES:
+                        if item.sell_in < self.__SELL_IN_LONG:
+                            if item.quality < self.__MAX_QUALITY:
                                 item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
+                        if item.sell_in < self.__SELL_IN_MID:
+                            if item.quality < self.__MAX_QUALITY:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != self.__HAND_OF_RAGNAROS:
                 item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.sell_in < self.__SELL_TODAY:
+                if item.name != self.__AGED_BRIE:
+                    if item.name != self.__BACKSTAGE_PASSES:
+                        if item.quality > self.__MIN_QUALITY:
+                            if item.name != self.__HAND_OF_RAGNAROS:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
+                    if item.quality < self.__MAX_QUALITY:
                         item.quality = item.quality + 1
 
 
