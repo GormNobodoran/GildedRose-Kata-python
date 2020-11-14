5c19eb70fef119b5f453b35be131bf0305c9de99
GormNobodoran <gremiodemke@yahoo.es>
2020-11-14 10:06:27 +0100

    06 - Addapted tests and used Item logic

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,31 +11,5 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != self.__AGED_BRIE and item.name != self.__BACKSTAGE_PASSES:
-                if item.isAboveMinQuality():
-                    if item.name != self.__HAND_OF_RAGNAROS:
-                        item.decreaseQuality()
-            else:
-                if item.isBelowMaxQuality():
-                    item.increaseQuality()
-                    if item.name == self.__BACKSTAGE_PASSES:
-                        if item.isBelowSellInMax():
-                            if item.isBelowMaxQuality():
-                                item.increaseQuality()
-                        if item.isBelowSellInMid():
-                            if item.isBelowMaxQuality():
-                                item.increaseQuality()
-            if item.name != self.__HAND_OF_RAGNAROS:
-                item.sell_in = item.sell_in - 1
-            if item.isBelowSellInMin():
-                if item.name != self.__AGED_BRIE:
-                    if item.name != self.__BACKSTAGE_PASSES:
-                        if item.isAboveMinQuality():
-                            if item.name != self.__HAND_OF_RAGNAROS:
-                                item.decreaseQuality()
-                    else:
-                        item.setQualityToMinimum()
-                else:
-                    if item.isBelowMaxQuality():
-                        item.increaseQuality()
+            item.update()
 
